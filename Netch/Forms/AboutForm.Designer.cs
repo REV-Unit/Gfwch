a5bb168da8e6bf3b989dbce3e33afc40ba0843cc
Rcmcpe <rcmcpe@qq.com>
2021-01-02 18:31:57 +0800

    添加REV Unit标识到关于

--- Netch/Forms/AboutForm.Designer.cs
+++ Netch/Forms/AboutForm.Designer.cs
@@ -29,41 +29,18 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(AboutForm));
-            this.SponsorGroupBox = new System.Windows.Forms.GroupBox();
-            this.SponsorPictureBox = new System.Windows.Forms.PictureBox();
             this.ChannelLabel = new System.Windows.Forms.LinkLabel();
             this.NetchPictureBox = new System.Windows.Forms.PictureBox();
-            this.SponsorGroupBox.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.SponsorPictureBox)).BeginInit();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.NetchPictureBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
-            // 
-            // SponsorGroupBox
-            // 
-            this.SponsorGroupBox.Controls.Add(this.SponsorPictureBox);
-            this.SponsorGroupBox.Location = new System.Drawing.Point(12, 235);
-            this.SponsorGroupBox.Name = "SponsorGroupBox";
-            this.SponsorGroupBox.Size = new System.Drawing.Size(314, 229);
-            this.SponsorGroupBox.TabIndex = 2;
-            this.SponsorGroupBox.TabStop = false;
-            this.SponsorGroupBox.Text = "Sponsor";
-            // 
-            // SponsorPictureBox
-            // 
-            this.SponsorPictureBox.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.SponsorPictureBox.Image = global::Netch.Properties.Resources.Sponsor;
-            this.SponsorPictureBox.Location = new System.Drawing.Point(6, 22);
-            this.SponsorPictureBox.Name = "SponsorPictureBox";
-            this.SponsorPictureBox.Size = new System.Drawing.Size(300, 200);
-            this.SponsorPictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
-            this.SponsorPictureBox.TabIndex = 1;
-            this.SponsorPictureBox.TabStop = false;
-            this.SponsorPictureBox.Click += new System.EventHandler(this.SponsorPictureBox_Click);
             // 
             // ChannelLabel
             // 
             this.ChannelLabel.AutoSize = true;
-            this.ChannelLabel.Location = new System.Drawing.Point(116, 215);
+            this.ChannelLabel.Location = new System.Drawing.Point(19, 135);
             this.ChannelLabel.Name = "ChannelLabel";
             this.ChannelLabel.Size = new System.Drawing.Size(113, 17);
             this.ChannelLabel.TabIndex = 5;
@@ -75,23 +52,43 @@
             // 
             this.NetchPictureBox.Cursor = System.Windows.Forms.Cursors.Hand;
             this.NetchPictureBox.Image = global::Netch.Properties.Resources.Netch;
-            this.NetchPictureBox.Location = new System.Drawing.Point(72, 12);
+            this.NetchPictureBox.Location = new System.Drawing.Point(12, 12);
             this.NetchPictureBox.Name = "NetchPictureBox";
-            this.NetchPictureBox.Size = new System.Drawing.Size(200, 200);
+            this.NetchPictureBox.Size = new System.Drawing.Size(120, 120);
             this.NetchPictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.NetchPictureBox.TabIndex = 0;
             this.NetchPictureBox.TabStop = false;
             this.NetchPictureBox.Click += new System.EventHandler(this.NetchPictureBox_Click);
             // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::Netch.Properties.Resources.REVUnit;
+            this.pictureBox1.Location = new System.Drawing.Point(152, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(120, 120);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 6;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(157, 135);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(111, 17);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "REV Unit, Rcmcpe";
+            // 
             // AboutForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(96F, 96F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
-            this.ClientSize = new System.Drawing.Size(338, 474);
+            this.ClientSize = new System.Drawing.Size(286, 167);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.ChannelLabel);
-            this.Controls.Add(this.SponsorGroupBox);
             this.Controls.Add(this.NetchPictureBox);
-            this.Font = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.Font = new System.Drawing.Font("Microsoft YaHei", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
@@ -100,9 +97,8 @@
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "About";
             this.Load += new System.EventHandler(this.AboutForm_Load);
-            this.SponsorGroupBox.ResumeLayout(false);
-            ((System.ComponentModel.ISupportInitialize)(this.SponsorPictureBox)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.NetchPictureBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -111,8 +107,8 @@
         #endregion
 
         private System.Windows.Forms.PictureBox NetchPictureBox;
-        private System.Windows.Forms.PictureBox SponsorPictureBox;
-        private System.Windows.Forms.GroupBox SponsorGroupBox;
         private System.Windows.Forms.LinkLabel ChannelLabel;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
     }
 }
